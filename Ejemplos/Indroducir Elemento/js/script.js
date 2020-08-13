71649bfb98a9167ead89a1ca11edec5f46f96031
Andryk Lara <lara.cordero@gmail.com>
2020-08-12 21:01:12 -0600

    Comentarios

--- Ejemplos/Indroducir Elemento/js/script.js
+++ Ejemplos/Indroducir Elemento/js/script.js
@@ -1,7 +1,11 @@
-//Creamos el elemento
+//Obtenemos el lugar donde queremos el nuevo elemento
 const elemento = document.getElementById('container');
+//Obtenemos la referencia del lugar posterior a donde queremos colocar el nuevo elemento
 const referencia = document.getElementById('referencia');
+//Creamos el nuevo elemento
 const nuevoElemento = document.createElement('p');
+//Le añadimos texto a ese nuevo elemento
 nuevoElemento.innerHTML ='Parrafo Insertado';
 elemento.appendChild(nuevoElemento);
+//Despues lo posicionamos donde lo queremos, usamos la referencia
 document.getElementById('container').insertBefore(nuevoElemento, referencia);
